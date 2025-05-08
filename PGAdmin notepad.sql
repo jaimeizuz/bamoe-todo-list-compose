3f9795025ff50abc2a779dfbda03fe60a9c78ff4
Jaime Izuzquiza <jizuzquiza@hotmail.es>
2025-05-08 09:39:17 +0200

    Lot of fixes

--- PGAdmin notepad.sql
+++ PGAdmin notepad.sql
@@ -18,3 +18,20 @@ insert into UserTask (id, name, processdefinitionid, processinstanceid, actualow
 select * from UserTask;
 select * from kogito.jbpm_user_tasks;
 select * from kogito.jbpm_user_tasks_metadata;
+
+create index if not exists idx_usertasks_tid on jbpm_user_tasks(user_task_id);
+create index if not exists idx_jbpm_user_tasks_deadline_tid on jbpm_user_tasks_deadline(task_id);
+create index if not exists idx_jbpm_user_tasks_inputs_tid on jbpm_user_tasks_inputs(task_id);
+create index if not exists idx_jbpm_user_tasks_outputs_tid on jbpm_user_tasks_outputs(task_id);
+create index if not exists idx_jbpm_user_tasks_metadata_tid on jbpm_user_tasks_metadata(task_id);
+create index if not exists idx_jbpm_user_tasks_admin_groups_tid on jbpm_user_tasks_admin_groups(task_id);
+create index if not exists idx_jbpm_user_tasks_admin_groups_gid on jbpm_user_tasks_admin_groups(group_id);
+create index if not exists idx_jbpm_user_tasks_admin_users_tid on jbpm_user_tasks_admin_users(task_id);
+create index if not exists idx_jbpm_user_tasks_admin_users_uid on jbpm_user_tasks_admin_users(user_id);
+create index if not exists idx_jbpm_user_tasks_attachments_tid on jbpm_user_tasks_attachments(task_id);
+create index if not exists idx_jbpm_user_tasks_potential_groups_tid on jbpm_user_tasks_potential_groups(task_id);
+create index if not exists idx_jbpm_user_tasks_potential_groups_gid on jbpm_user_tasks_potential_groups(group_id);
+create index if not exists idx_jbpm_user_tasks_potential_users_tid on jbpm_user_tasks_potential_users(task_id);
+create index if not exists idx_jbpm_user_tasks_potential_users_uid on jbpm_user_tasks_potential_users(user_id);
+create index if not exists idx_jbpm_user_tasks_excluded_users_uid on jbpm_user_tasks_excluded_users(user_id);
+create index if not exists idx_jbpm_user_tasks_excluded_users_tid on jbpm_user_tasks_excluded_users(task_id);
